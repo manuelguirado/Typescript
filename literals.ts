eee385d81d20b17894826268d505e425fbf64956
manuel <manu22.mgb@gmail.com>
2024-10-13 13:10:33 +0200

    learning literals

---
+++ literals.ts
@@ -0,0 +1,16 @@
+//making a function only accepts literals and specific values like "asc", "desc"
+type Order = "asc" | "desc";
+function sortArray(order: Order) : void{
+    if(order === "asc"){
+        console.log("ascending");
+         let numbers : number[] = [2,5,3,7,1];
+        console.log(numbers.sort());
+    }else{
+        console.log("descending");
+        let numbers : number[] = [2,5,3,7,1];
+        console.log(numbers.sort().reverse());
+    }
+
+}
+sortArray("asc");
+sortArray("desc");
