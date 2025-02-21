c2e1bd10b40871c718961632b662ee0b3739e07b
manuelguirado <manu22.mgb@gmail.com>
2025-02-21 17:50:17 +0100

    learning about utility types

--- utilityTypes.ts
+++ utilityTypes.ts
@@ -1,11 +1,52 @@
+interface cats {
+    age : number;
+    breed : string;
+}
 interface usuario {
  id: number;
  name : string;
  email : String;
 
 }
+interface props {
+    a?: string;
+    b?: string;
+
+}
+interface Todo {
+    title : string,
+    description : string
+    completed : boolean
+}
+type Todopreview = Pick<Todo, "title" | "completed">; //pick only title and completed properties
+let todo : Todopreview = {
+    title : "Clean room",
+    completed : false
+}
+
+
+const obj : props = {
+    a : "a"
+}
+const obj2 : Required<props> = { //all properties are required
+    a : "a",
+    b : "b"
+
+}
+type catName = "miffy" | "boris" | "mordred";
+let  cats : Record<catName, cats> = { //create a record type with the catName as key and cats as value
+    miffy : {age : 10, breed : "Persian"},
+    boris : {age : 5, breed : "Maain"},
+    mordred : {age : 16, breed : "Siamese"}
+
+}
 type usuarioPartila = Partial<usuario>; // all properties are optional
 type usuariosinEmail = Omit<usuario, "email">; // omit email property
+type awaitedtype = Awaited<Promise<String>>; // await a promise type 
+let pokeAPI : string = "https://pokeapi.co/api/v2/pokemon";
+let promise : Promise<String> = new Promise((resolve, reject) => {
+    resolve(pokeAPI);
+})
 
 let user : usuario = {
     id : 1,
@@ -17,4 +58,12 @@ let userWithoutEmail : usuariosinEmail = {
     name : "John"
 
 }
+let userPartial : usuarioPartila= {
+    
+    name : "John"
+}
 console.log(userWithoutEmail);
+console.log(userPartial);
+console.log(promise ) 
+console.log(cats);
+console.log(todo);
